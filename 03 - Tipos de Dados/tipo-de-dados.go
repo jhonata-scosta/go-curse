564930792b4716d39abc62a300a109d951e3e8be
Jhonata Souza <jhonata.scosta@proton.me>
2026-02-06 23:06:34 -0300

    class: Data types

---
+++ 03 - Tipos de Dados/tipo-de-dados.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* @@ Aula - Tipo de Dados
+
+		*Tipos basicos em Go*
+
+		Tipos Numéricos
+			- Numéros Inteiro (int):
+					Os numeros tipo int8 só suportam numeros até 8bits e sucessivamente até o 64bits 
+					Inteiros: int, int8, int16, int32, int64
+					O tipo *int* ele vai usar a arquitetura da máquina que está sendo utilizada para
+					definir o tamanho do tipo. Por exemplo, um PC 32 bits ele vai ter o tamanho de int32.
+			- Numeros Inteiros Sem Sinal "unsigned" (uint)
+					Os numeros tipo uint8 só suportam numeros até 8bits positvos e sucessivamente até o 64bits
+					Unsigned: uint, uint8, uint16, uint32, uint64
+					O tipo *uint* e
+		
+	*/
+	var numero int = 10000000
+	fmt.Println(numero)
+	
+}
