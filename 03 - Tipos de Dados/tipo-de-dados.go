ce382a1ef629332455325d6ff7cb8333155387f2
Jhonata Souza <jhonata.scosta@proton.me>
2026-02-06 23:14:09 -0300

    class: Data types 2

--- 03 - Tipos de Dados/tipo-de-dados.go
+++ 03 - Tipos de Dados/tipo-de-dados.go
@@ -16,8 +16,9 @@ func main() {
 			- Numeros Inteiros Sem Sinal "unsigned" (uint)
 					Os numeros tipo uint8 só suportam numeros até 8bits positvos e sucessivamente até o 64bits
 					Unsigned: uint, uint8, uint16, uint32, uint64
-					O tipo *uint* e
-		
+					O tipo *uint* ele vai usar a arquitetura de máquina que esta sendo utilizada para
+					definir o tamanho do tipo. Por exemplo, um PC 64 bits ele vai ter o tamanho de uint64
+			- 
 	*/
 	var numero int = 10000000
 	fmt.Println(numero)
