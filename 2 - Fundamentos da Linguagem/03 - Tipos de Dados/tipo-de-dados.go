23126ab76d02ac10c1039d33de35052468ef1041
Jhonata Souza <jhonata.scosta@proton.me>
2026-02-08 10:10:00 -0300

    Class: Data types 5

--- 2 - Fundamentos da Linguagem/03 - Tipos de Dados/tipo-de-dados.go
+++ 2 - Fundamentos da Linguagem/03 - Tipos de Dados/tipo-de-dados.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	/* @@ Aula - Tipo de Dados
@@ -13,17 +16,26 @@ func main() {
 					Inteiros: int, int8, int16, int32, int64
 					O tipo *int* ele vai usar a arquitetura da máquina que está sendo utilizada para
 					definir o tamanho do tipo. Por exemplo, um PC 32 bits ele vai ter o tamanho de int32.
-			- Numeros Inteiros Sem Sinal "unsigned" (uint)
+			- Numeros Inteiros Sem Sinal "unsigned" (uint):
 					Os numeros tipo uint8 só suportam numeros até 8bits positvos e sucessivamente até o 64bits
 					Unsigned: uint, uint8, uint16, uint32, uint64
 					O tipo *uint* ele vai usar a arquitetura de máquina que esta sendo utilizada para
 					definir o tamanho do tipo. Por exemplo, um PC 64 bits ele vai ter o tamanho de uint64
-			- Numeros Reais (float)
+			- Numeros Reais (float):
 					São numeros com ponto flutuante, porém só suportam dois tipos: 32 e 64bits
 					Reais: float32 e float64
 					O tipo aceita numeros com pontos flutantes, por exemplo: 720.30 não aceitam virgula,
 					apenas ponto. (.)
 					Também não é possivel definir o tipo float, apenas os seus tipos.
+			- Cadeia de Caracteres (string):
+					É o tipo de texto da linguagem, deve ser escrito entras aspas duplas ("").
+					Para o tipo char(unico caracter), ele utilizar o tipo string, mas o caracter precisa 
+					está entre aspas simples (''), e ele irá retornar o valor daquele caracter da tabela
+					ASCII e será do tipo int.
+			- Booleano(bool):
+					É o tipo que possui apenas 2 valores, verdadeiro(true) e falso (false).
+			- Erro (error):
+					É um tipo que o possui para lidar com erros, junto com o pacote padrão errors.
 	*/ 
 
 	// Numeros inteiros
@@ -52,4 +64,33 @@ func main() {
 	numeroReal3 := 54448.23
 	fmt.Println(numeroReal3)
 
+	//Strings (Cadeia de Caracteres)
+	var str string = "Texto"
+	fmt.Println(str)
+	char := "A"
+	fmt.Println(char)
+
+	//Booleano
+
+	var boolean bool = true
+	fmt.Println(boolean)
+
+	// Error
+
+	var erro error = errors.New("Erro Interno")
+	fmt.Println(erro)
+
+	/* Todos os tipos possui um valor padrão. Quando uma variavel é inicializada, porém sem ter
+	valor, ela já possui um valor proprio para seu tipo.
+	Ex: 
+		# var str2 string 
+		retorna um string vazio
+		# var numb int
+		retorna um 0
+
+		# var bol bool
+		retorna o valor false
+	*/
+	var str2 string
+	fmt.Println(str2)
 }
