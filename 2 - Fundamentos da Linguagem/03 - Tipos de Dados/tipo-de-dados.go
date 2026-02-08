ef02f15387213e85bbff58ffa932064d6b1954a8
Jhonata Souza <jhonata.scosta@proton.me>
2026-02-08 10:00:00 -0300

    class: Data types 3

--- 2 - Fundamentos da Linguagem/03 - Tipos de Dados/tipo-de-dados.go
+++ 2 - Fundamentos da Linguagem/03 - Tipos de Dados/tipo-de-dados.go
@@ -18,9 +18,37 @@ func main() {
 					Unsigned: uint, uint8, uint16, uint32, uint64
 					O tipo *uint* ele vai usar a arquitetura de máquina que esta sendo utilizada para
 					definir o tamanho do tipo. Por exemplo, um PC 64 bits ele vai ter o tamanho de uint64
-			- 
+			- Numeros Reais (float)
+					São numeros com ponto flutuante, porém só suportam dois tipos: 32 e 64bits
+					Reais: float32 e float64
+					O tipo aceita numeros com pontos flutantes, por exemplo: 720.30 não aceitam virgula,
+					apenas ponto. (.)
+					Também não é possivel definir o tipo float, apenas os seus tipos.
 	*/
-	var numero int = 10000000
+
+	// Numeros inteiros
+	var numero int = -10000000
 	fmt.Println(numero)
+	var numero2 uint32 = 100000
+	fmt.Println(numero2)
+
+	//Alias
+	//Alias de int32
+	var numero3 rune = 552056
+	fmt.Println(numero3)
+	//Alias de uint8
+	var numero4 byte = 123
+	fmt.Println(numero4)
 	
+
+	//Numeros Reais
+	var numeroReal1 float32 = 123.44
+	fmt.Println(numeroReal1)
+
+	var numeroReal2 float64 = 7256.32
+	fmt.Println(numeroReal2)
+
+	numeroReal3 := 54448.23
+	fmt.Println(numeroReal3)
+
 }
