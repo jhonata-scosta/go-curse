e690093dcd74e999f8035e7731dc40581a490689
Jhonata Souza <jhonata.scosta@proton.me>
2026-02-09 10:20:00 -0300

    Class: Functions 3

--- 2 - Fundamentos da Linguagem/04 - Funções/main.go
+++ 2 - Fundamentos da Linguagem/04 - Funções/main.go
@@ -14,14 +14,20 @@ import "fmt"
 func somar(n1 int8, n2 int8) int8 {
 	return n1 + n2
 }
+/* 2 - É possivel receber mais de dois tipos de retorno, precisando especificar cada uma delas 
+	entre parenteses. 
+*/
+func calculosMatematicos(n1, n2 int8) (int8, int8){
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
 
 func main() {
 	// 1 - aqui ele é chamado e o valor final fica armazenado na variavel
 	soma := somar(8, 6)
 	fmt.Println(soma)
-	/* É possivel contruir uma função da seguinte maneira. Pois a função também é um tipo
-
-	*/
+	/* É possivel contruir uma função da seguinte maneira. Pois a função também é um tipo */
 	var f = func(txt string) string {
 		fmt.Println(txt)
 		return txt
@@ -29,5 +35,8 @@ func main() {
 
 	resultado := f("Texto da função 1")
 	fmt.Println(resultado)
+
+	resultadoSoma, resultadoSubtracao := calculosMatematicos(10, 15)
+	fmt.Println(resultadoSoma, resultadoSubtracao)
 }
 
