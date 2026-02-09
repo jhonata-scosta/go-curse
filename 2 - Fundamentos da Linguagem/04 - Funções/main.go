f83be1d580bfc801bbad1b3d8091e04da9f60307
Jhonata Souza <jhonata.scosta@proton.me>
2026-02-09 10:10:00 -0300

    Class: Functions

---
+++ 2 - Fundamentos da Linguagem/04 - Funções/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+/*
+	@@ Aula - Funções
+	Funções são blocos de texto reutilizaveis para realizar tarefas especificas.
+	Em go para declarar uma função utilizamos a palavra reservada "func" e em sua estrutura podemos
+	receber parametros, tipos de retorno e o codigo que gere a logica que será utilizada.
+	Exemplos:
+*/
+
+// 1 - Essa função recebe dois paramentros do tipo int8 e seu retorno também e do tipo int8:
+func somar(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+func main() {
+	// 1 - aqui ele é chamado e o valor final fica armazenado na variavel
+	soma := somar(8, 6)
+	fmt.Println(soma)
+	/* É possivel contruir uma função da seguinte maneira. Pois a função também é um tipo
+
+	*/
+	var f = func(txt string) {
+		fmt.Println("Função F")
+	}
+
+	f("Texto da função 1")
+}
+
